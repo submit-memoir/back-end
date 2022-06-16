7aa9a56afcba7804a1adc2b7b566c59c6e5c49b4
min <minchae571@gmail.com>
2022-06-16 22:33:13 +0900

    [ ADD ] 칼럼 추가 ( createdAt )

--- src/main/java/com/min56/backend/entity/memoir/Memoir.java
+++ src/main/java/com/min56/backend/entity/memoir/Memoir.java
@@ -3,15 +3,20 @@ package com.min56.backend.entity.memoir;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Memoir {
 
@@ -37,15 +42,17 @@ public class Memoir {
     @Column(length = 15, nullable = false)
     private String userId;
 
+    @CreatedDate
+    private static LocalDate createdAt;
+
     public static Memoir createMemoir(String title, String goal, String learned, String felt, String nextGoal, String userId) {
         Memoir memoir = new Memoir();
-        memoir.title = title;
+        memoir.title = title; // Memoir객체안에 title값을 넣는다..?
         memoir.goal = goal;
         memoir.learned = learned;
         memoir.felt = felt;
         memoir.nextGoal = nextGoal;
         memoir.userId = userId;
-
         return memoir;
     }
 }
